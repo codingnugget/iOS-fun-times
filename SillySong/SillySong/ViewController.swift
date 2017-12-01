828457d9c9f007a735602be1ebea437f3c34beb0
Ryan Nguyen <link12155@yahoo.com>
2017-12-01 14:45:14 -0600

    added functions from playground, attempt at implementing constraints

--- SillySong/SillySong/ViewController.swift
+++ SillySong/SillySong/ViewController.swift
@@ -29,8 +29,62 @@ class ViewController: UIViewController {
     }
     //takes text from namefield and generate lyrics in lyrics textview
     @IBAction func displayLyrics(_ sender: Any) {
-    
+        //set lyrics to the textview
+        lyricsView.text! = lyricsFromName(lyricsTemplate: bananaTemplate, fullName: nameField.text!)
     }
     
 }
 
+//test lyrics
+let bananaTemplate = ["<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+                      "Banana Fana Fo F<SHORT_NAME>",
+                      "Me My Mo M<SHORT_NAME>",
+                      "<FULL_NAME>"].joined(separator: "\n")
+
+//creates the flow in lyrics. returns name in all lowercase, and remove any consonents (if they exist) before the first vowel
+func shortenName(name:String)->String
+{
+    var lowercaseName = name.lowercased()
+    let vowels = CharacterSet(charactersIn: "aeiou")
+    
+    //create index for substring
+    let indexStart = lowercaseName.index(lowercaseName.startIndex, offsetBy: 1)
+    let nameWithoutBeginningCharacter = lowercaseName[indexStart...]
+    let beginningCharacter = lowercaseName.index(lowercaseName.endIndex,offsetBy:0)
+    
+    //check for empty string
+    if(lowercaseName == "")
+    {
+        return ""
+    }
+    
+    //check if first character in name contains a vowel
+    if(String(lowercaseName[lowercaseName.startIndex]).rangeOfCharacter(from: vowels) != nil)
+    {
+        //if vowel is the first character, simply return the name
+        return String(lowercaseName)
+    }
+    else
+    {
+        //iterate through the name until the first vowel
+        for index in lowercaseName.indices
+        {
+            //check if current index is a vowel
+            if(String(lowercaseName[index]).rangeOfCharacter(from: vowels) != nil)
+            {
+                return String(lowercaseName[index...])
+            }
+        }
+        //should never reach here
+        return "vowels were not found"
+    }
+}
+
+//function with the lyric template
+func lyricsFromName(lyricsTemplate: String, fullName: String) -> String
+{
+    let shortenedName = shortenName(name: fullName)
+    //replace full and short names in lyrics template
+    let lyrics = lyricsTemplate.replacingOccurrences(of: "<FULL_NAME>", with: shortenedName).replacingOccurrences(of: "<SHORT_NAME>", with: fullName)
+    return lyrics
+}
